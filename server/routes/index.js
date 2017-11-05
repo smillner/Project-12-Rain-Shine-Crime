63d27f874942bd34d5948789c91edd154efc837c
scottmillner <scottmillner237@gmail.com>
2017-11-05 11:51:28 -0700

    proxy

--- server/routes/index.js
+++ server/routes/index.js
@@ -41,7 +41,7 @@ router.get('/api/crime/:latitude/:longitude', (req, res, next) => {
 
 	request(`https://api.spotcrime.com/crimes.json?lat=${latitude}&lon=${longitude}&radius=0.02&key=privatekeyforspotcrimepublicusers-commercialuse-877.410.1607&_=1509493804097`,
 	{
-		"proxy": "http://scooter237:taco1234@us-wa.proxymesh.com:31280"
+		'proxy': 'http://scooter237:taco1234@us-wa.proxymesh.com:31280'
 	}
 		(err, res, body) => {
 			res.send(body);
