083c73d3771c36b6869925942381ccb237944a17
smillner <34517346+smillner@users.noreply.github.com>
2018-02-14 14:50:30 -0700

    Update crime.service.ts

--- src/app/services/crime.service.ts
+++ src/app/services/crime.service.ts
@@ -10,8 +10,8 @@ export class CrimeService {
 
 	// GET request to crime API for crime data for selected city
 	getCrimeData(coordinates: Array<number>) { 
-		//return this.httpClient.get(`https://api.spotcrime.com/crimes.json?lat=${coordinates[0]}&lon=${coordinates[1]}&radius=0.02&callback=jQuery213027649028043821566_1509493804094&key=privatekeyforspotcrimepublicusers-commercialuse-877.410.1607&_=1509493804097`,  {responseType: 'text'});
-	    return this.httpClient.get(`/api/crime/${coordinates[0]}/${coordinates[1]}`, {responseType: 'text'});
+		return this.httpClient.get(`https://api.spotcrime.com/crimes.json?lat=${coordinates[0]}&lon=${coordinates[1]}&radius=0.02&callback=jQuery213027649028043821566_1509493804094&key=privatekeyforspotcrimepublicusers-commercialuse-877.410.1607&_=1509493804097`,  {responseType: 'text'});
+	    //return this.httpClient.get(`/api/crime/${coordinates[0]}/${coordinates[1]}`, {responseType: 'text'});
 	  } 
 
 }
